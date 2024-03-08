30ea13e35cb5878561770afea30e54f8a0305e83
Aza Faiz Hamdani <azafaiz746@gmail.com>
2024-03-08 22:03:35 +0700

    upload tugas landing page dan javascript dasar

---
+++ dasar.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  let text;
+  if (confirm("Press a button!") == true) {
+    alert("anda beli");
+  } else {
+    alert("anda tidak jadi beli");
+  }
+}
---
+++ jsdasar.js
@@ -0,0 +1,15 @@
+// 1
+var nama = "Dani";
+document.write(nama);
+document.write("<br>");
+document.write("<br>");
+// 2
+var nama = "Dani";
+nama = "pedro";
+document.write(nama);
+document.write("<br>");
+document.write("<br>");
+//  3
+let namaP = "jokrori";
+namaP = "Bowo gemoy";
+document.write(namaP);
